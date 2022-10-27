3994c2823229764f0d40097dffee49a68a6b779c
Christian Koch <christian.koch@mail.schwarz>
2022-10-27 11:48:18 +0200

    initial commit

---
+++ pages/dessert.js
@@ -0,0 +1,8 @@
+export default function Dessert() {
+  return (
+    <div>
+      <h2>DessertPage</h2>
+      <p>Hier gibt den Nachtisch</p>
+    </div>
+  );
+}
---
+++ pages/veggie.js
@@ -0,0 +1,8 @@
+export default function Veggie() {
+  return (
+    <div>
+      <h2>Veggie Page</h2>
+      <p>Hier gibt vegetarisch</p>
+    </div>
+  );
+}
